b5dd66859dd78fd674b9673fa5fd74d878558d0e
Malank Goel <goelmalank10@gmail.com>
2026-06-18 23:57:12 -0500

    Expose Kalshi environment base URLs

--- kalshi_auto_trader/kalshi/__init__.py
+++ kalshi_auto_trader/kalshi/__init__.py
@@ -1,6 +1,6 @@
-"""Kalshi API integrations."""
+"""Public interface for exchange-wide Kalshi integrations."""
 
 from kalshi_auto_trader.kalshi.client import KalshiClient
+from kalshi_auto_trader.settings import DEMO_BASE_URL, PROD_BASE_URL
 
-__all__ = ["KalshiClient"]
-
+__all__ = ["DEMO_BASE_URL", "KalshiClient", "PROD_BASE_URL"]

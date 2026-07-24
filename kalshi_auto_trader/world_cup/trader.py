a74e8014eedac0105d41bb19068a711102dd486e
Malank Goel <goelmalank10@gmail.com>
2026-07-24 01:21:08 -0500

    Add no ask skip constant

--- kalshi_auto_trader/world_cup/trader.py
+++ kalshi_auto_trader/world_cup/trader.py
@@ -42,8 +42,10 @@ from kalshi_auto_trader.world_cup import config, markets as mm, model
 
 SKIP_NO_STAKE = "no +EV Kelly stake"
 SKIP_NO_MARKET = "no live market"
+SKIP_NO_ASK = "no ask price"
 
 __all__ = [
+    "SKIP_NO_ASK",
     "SKIP_NO_MARKET",
     "SKIP_NO_STAKE",
     "actionable_plans",

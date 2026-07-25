dc15a36b0c3bf65243ffbb72827ed4bf18398773
Malank Goel <goelmalank10@gmail.com>
2026-07-25 16:26:31 -0500

    Assert market index template export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -98,6 +98,7 @@ def test_shared_modules_export_expected_entry_points():
             "event_token",
             "first_price_cents",
             "is_two_five_line",
+            "market_index_template",
             "market_text",
             "resolve_order",
             "team_key",

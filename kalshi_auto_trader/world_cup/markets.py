706363a269a3698b0b5616292f0265eb68d65290
Malank Goel <goelmalank10@gmail.com>
2026-07-29 16:49:46 -0500

    Use winner draw price key in odds row

--- kalshi_auto_trader/world_cup/markets.py
+++ kalshi_auto_trader/world_cup/markets.py
@@ -307,7 +307,7 @@ def build_odds_row(idx: dict, home: str, away: str) -> dict:
 
     put(WINNER_HOME_PRICE_KEY, idx["winner"].get(_norm(home)))
     put(WINNER_AWAY_PRICE_KEY, idx["winner"].get(_norm(away)))
-    put("winner_draw_price", idx.get("draw"))
+    put(WINNER_DRAW_PRICE_KEY, idx.get("draw"))
     put("over_2_5_price", idx.get("over"))
     put("btts_yes_price", idx.get("btts"))
     return row

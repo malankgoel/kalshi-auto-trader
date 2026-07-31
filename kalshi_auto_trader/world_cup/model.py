f8e5887d86b2ec77518b95b7fc83e28ca7ad396d
Malank Goel <goelmalank10@gmail.com>
2026-07-31 16:06:41 -0500

    Use shared BTTS no price key in model

--- kalshi_auto_trader/world_cup/model.py
+++ kalshi_auto_trader/world_cup/model.py
@@ -284,7 +284,10 @@ def flag_bets(game: dict, odds: dict) -> list[Bet]:
         if b:
             bets.append(b)
 
-    by, bn = _c2p(odds.get(markets.BTTS_YES_PRICE_KEY)), _c2p(odds.get("btts_no_price"))
+    by, bn = (
+        _c2p(odds.get(markets.BTTS_YES_PRICE_KEY)),
+        _c2p(odds.get(markets.BTTS_NO_PRICE_KEY)),
+    )
     if by is not None:
         b = _evaluate("btts", "BTTS YES", "BTTS NO", "",
                       game["model_btts"], devig_binary(by, bn), by,

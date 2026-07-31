10fb4371877737999050936f6537f6e3d64345bc
Malank Goel <goelmalank10@gmail.com>
2026-07-31 16:05:50 -0500

    Use shared away price key in model

--- kalshi_auto_trader/world_cup/model.py
+++ kalshi_auto_trader/world_cup/model.py
@@ -260,7 +260,7 @@ def flag_bets(game: dict, odds: dict) -> list[Bet]:
 
     hp, dp, ap = (_c2p(odds.get(markets.WINNER_HOME_PRICE_KEY)),
                   _c2p(odds.get(markets.WINNER_DRAW_PRICE_KEY)),
-                  _c2p(odds.get("winner_away_price")))
+                  _c2p(odds.get(markets.WINNER_AWAY_PRICE_KEY)))
     if None not in (hp, dp, ap):
         fh, fd, fa = devig_three_way(hp, dp, ap)
         for model_p, fair_p, raw_p, team, label in (

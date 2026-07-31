2cacc4289c22cd720356a4a0da0ba1098da7bd77
Malank Goel <goelmalank10@gmail.com>
2026-07-31 16:04:12 -0500

    Assert under 2.5 price key export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -97,6 +97,7 @@ def test_shared_modules_export_expected_entry_points():
             "NO_ASK_PRICE_FIELDS",
             "NO_BID_PRICE_FIELDS",
             "OVER_2_5_PRICE_KEY",
+            "UNDER_2_5_PRICE_KEY",
             "YES_ASK_PRICE_FIELDS",
             "YES_BID_PRICE_FIELDS",
             "WINNER_AWAY_PRICE_KEY",

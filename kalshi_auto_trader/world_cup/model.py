17028fcb379d78356ae73698715789973c119cea
Malank Goel <goelmalank10@gmail.com>
2026-08-04 19:25:05 -0500

    Use BTTS yes selection constant

--- kalshi_auto_trader/world_cup/model.py
+++ kalshi_auto_trader/world_cup/model.py
@@ -314,7 +314,7 @@ def flag_bets(game: dict, odds: dict) -> list[Bet]:
         _c2p(odds.get(markets.BTTS_NO_PRICE_KEY)),
     )
     if by is not None:
-        b = _evaluate(BTTS_LINE, "BTTS YES", "BTTS NO", "",
+        b = _evaluate(BTTS_LINE, BTTS_YES_SELECTION, "BTTS NO", "",
                       game["model_btts"], devig_binary(by, bn), by,
                       bn if bn is not None else probability.probability_complement(by))
         if b:

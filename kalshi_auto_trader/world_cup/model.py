f00d375740c88f21365a67525df1ac1950fe0099
Malank Goel <goelmalank10@gmail.com>
2026-08-05 20:54:03 -0500

    Use model home win key

--- kalshi_auto_trader/world_cup/model.py
+++ kalshi_auto_trader/world_cup/model.py
@@ -309,7 +309,7 @@ def flag_bets(game: dict, odds: dict) -> list[Bet]:
     if None not in (hp, dp, ap):
         fh, fd, fa = devig_three_way(hp, dp, ap)
         for model_p, fair_p, raw_p, team, label in (
-            (game["model_home_win"], fh, hp, game["home_team"], game["home_team"]),
+            (game[MODEL_HOME_WIN_KEY], fh, hp, game["home_team"], game["home_team"]),
             (game["model_draw"], fd, dp, "", "Draw"),
             (game["model_away_win"], fa, ap, game["away_team"], game["away_team"]),
         ):

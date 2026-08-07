8ee38c9d4fac888ce92cef6e779018c19dce6dd7
Malank Goel <goelmalank10@gmail.com>
2026-08-07 17:02:58 -0500

    Assert game row keys export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -109,6 +109,7 @@ def test_shared_modules_export_expected_entry_points():
             "flag_bets",
             "fixture_metadata_keys",
             "fixture_team_keys",
+            "game_row_keys",
             "game_key",
             "matches_fixture",
             "meets_edge_threshold",

07739423f295d9b683bb249f9857a50f0ca19e9a
Malank Goel <goelmalank10@gmail.com>
2026-08-08 22:45:13 -0500

    Assert prediction BTTS key export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -102,6 +102,7 @@ def test_shared_modules_export_expected_entry_points():
             "NO_SELECTION_PREFIX",
             "OVER_2_5_SELECTION",
             "PRED_AWAY_WIN_KEY",
+            "PRED_BTTS_KEY",
             "PRED_DRAW_KEY",
             "PRED_HOME_WIN_KEY",
             "PRED_OVER_2_5_KEY",

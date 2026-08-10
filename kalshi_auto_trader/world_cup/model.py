c7903f1be1f06a5495e0b42b095d452b99c10e69
Malank Goel <goelmalank10@gmail.com>
2026-08-09 22:27:53 -0400

    Add prediction group key

--- kalshi_auto_trader/world_cup/model.py
+++ kalshi_auto_trader/world_cup/model.py
@@ -41,6 +41,7 @@ __all__ = [
     "PRED_AWAY_WIN_KEY",
     "PRED_BTTS_KEY",
     "PRED_DRAW_KEY",
+    "PRED_GROUP_KEY",
     "PRED_HOME_WIN_KEY",
     "PRED_MATCH_ID_KEY",
     "PRED_OVER_2_5_KEY",
@@ -96,6 +97,7 @@ NO_SELECTION_PREFIX = "NO"
 PRED_AWAY_WIN_KEY = "away_win"
 PRED_BTTS_KEY = "both_teams_to_score"
 PRED_DRAW_KEY = "draw"
+PRED_GROUP_KEY = "group"
 PRED_HOME_WIN_KEY = "home_win"
 PRED_MATCH_ID_KEY = "match_id"
 PRED_OVER_2_5_KEY = "over_2_5"

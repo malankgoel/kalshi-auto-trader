1af2e5bfb05b30ed9592d320720622af10919998
Malank Goel <goelmalank10@gmail.com>
2026-08-13 18:31:29 -0400

    Add prediction away team key

--- kalshi_auto_trader/world_cup/model.py
+++ kalshi_auto_trader/world_cup/model.py
@@ -38,6 +38,7 @@ __all__ = [
     "MODEL_HOME_WIN_KEY",
     "MODEL_OVER_2_5_KEY",
     "NO_SELECTION_PREFIX",
+    "PRED_AWAY_TEAM_KEY",
     "PRED_AWAY_WIN_KEY",
     "PRED_BTTS_KEY",
     "PRED_DRAW_KEY",
@@ -104,6 +105,7 @@ MODEL_DRAW_KEY = "model_draw"
 MODEL_HOME_WIN_KEY = "model_home_win"
 MODEL_OVER_2_5_KEY = "model_over_2_5"
 NO_SELECTION_PREFIX = "NO"
+PRED_AWAY_TEAM_KEY = "away_team"
 PRED_AWAY_WIN_KEY = "away_win"
 PRED_BTTS_KEY = "both_teams_to_score"
 PRED_DRAW_KEY = "draw"

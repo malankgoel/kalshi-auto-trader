5b77c508538bb2e7f1cbdc279bf3701fc22fde70
Malank Goel <goelmalank10@gmail.com>
2026-08-13 18:32:07 -0400

    Assert prediction away team key export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -101,6 +101,7 @@ def test_shared_modules_export_expected_entry_points():
             "MODEL_OVER_2_5_KEY",
             "NO_SELECTION_PREFIX",
             "OVER_2_5_SELECTION",
+            "PRED_AWAY_TEAM_KEY",
             "PRED_AWAY_WIN_KEY",
             "PRED_BTTS_KEY",
             "PRED_DRAW_KEY",

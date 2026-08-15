c2c6a45901699e0bfef3ac8873ac84c32461aff6
Malank Goel <goelmalank10@gmail.com>
2026-08-14 21:12:40 -0400

    Assert prediction BTTS value key export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -104,6 +104,7 @@ def test_shared_modules_export_expected_entry_points():
             "PRED_AWAY_TEAM_KEY",
             "PRED_AWAY_WIN_KEY",
             "PRED_BTTS_KEY",
+            "PRED_BTTS_VALUE_KEY",
             "PRED_DRAW_KEY",
             "PRED_GROUP_KEY",
             "PRED_HOME_TEAM_KEY",

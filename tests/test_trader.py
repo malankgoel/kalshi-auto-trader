a3dd4bf0d6759d4fb40805dce04b0e4724664279
Malank Goel <goelmalank10@gmail.com>
2026-08-16 00:36:28 -0400

    Cover prediction over 2.5 value key

--- tests/test_trader.py
+++ tests/test_trader.py
@@ -273,6 +273,10 @@ def test_prediction_over_2_5_key_names_source_field():
     assert model.PRED_OVER_2_5_KEY == "over_2_5"
 
 
+def test_prediction_over_2_5_value_key_names_loaded_field():
+    assert model.PRED_OVER_2_5_VALUE_KEY == "over_2_5"
+
+
 def test_prediction_under_2_5_key_names_source_field():
     assert model.PRED_UNDER_2_5_KEY == "under_2_5"
 

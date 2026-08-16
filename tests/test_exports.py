fa83872c874b37d829147946aeef1696ff4caa16
Malank Goel <goelmalank10@gmail.com>
2026-08-16 00:33:57 -0400

    Assert prediction home win value key export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -108,6 +108,7 @@ def test_shared_modules_export_expected_entry_points():
             "PRED_DRAW_KEY",
             "PRED_GROUP_KEY",
             "PRED_HOME_TEAM_KEY",
+            "PRED_HOME_WIN_VALUE_KEY",
             "PRED_HOME_WIN_KEY",
             "PRED_MATCH_ID_KEY",
             "PRED_OVER_2_5_KEY",

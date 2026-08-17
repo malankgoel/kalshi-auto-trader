0b3781c0a3f496ba4bfdc9e722424566ac7003a0
Malank Goel <goelmalank10@gmail.com>
2026-08-17 10:25:44 -0400

    Add prediction group value key

--- kalshi_auto_trader/world_cup/model.py
+++ kalshi_auto_trader/world_cup/model.py
@@ -45,6 +45,7 @@ __all__ = [
     "PRED_BTTS_VALUE_KEY",
     "PRED_DRAW_VALUE_KEY",
     "PRED_DRAW_KEY",
+    "PRED_GROUP_VALUE_KEY",
     "PRED_GROUP_KEY",
     "PRED_HOME_TEAM_KEY",
     "PRED_HOME_WIN_VALUE_KEY",
@@ -126,6 +127,7 @@ PRED_BTTS_KEY = "both_teams_to_score"
 PRED_BTTS_VALUE_KEY = "btts"
 PRED_DRAW_VALUE_KEY = "draw"
 PRED_DRAW_KEY = "draw"
+PRED_GROUP_VALUE_KEY = "group"
 PRED_GROUP_KEY = "group"
 PRED_HOME_TEAM_KEY = "home_team"
 PRED_HOME_WIN_VALUE_KEY = "home_win"

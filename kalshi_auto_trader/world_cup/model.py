89105679521b8a1a90308bbb6d49d1ebc7911949
Malank Goel <goelmalank10@gmail.com>
2026-08-20 12:20:12 -0500

    Use prediction model values helper for game lookup

--- kalshi_auto_trader/world_cup/model.py
+++ kalshi_auto_trader/world_cup/model.py
@@ -429,9 +429,7 @@ def find_game(match_id: str = "", home: str = "", away: str = "") -> Optional[di
         return {
             MATCH_ID_KEY: pred[PRED_MATCH_ID_VALUE_KEY], DATE_KEY: schedule_row_date(sc) if sc else "",
             KICKOFF_UTC_KEY: ko, GROUP_KEY: pred[PRED_GROUP_VALUE_KEY], HOME_TEAM_KEY: h, AWAY_TEAM_KEY: a,
-            MODEL_HOME_WIN_KEY: pred[PRED_HOME_WIN_VALUE_KEY], MODEL_DRAW_KEY: pred[PRED_DRAW_VALUE_KEY],
-            MODEL_AWAY_WIN_KEY: pred[PRED_AWAY_WIN_VALUE_KEY], MODEL_OVER_2_5_KEY: pred[PRED_OVER_2_5_VALUE_KEY],
-            MODEL_BTTS_KEY: pred[PRED_BTTS_VALUE_KEY],
+            **prediction_model_values(pred),
         }
     return None
 

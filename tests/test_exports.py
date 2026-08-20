68c30c7017a317a8dcb5c22e4bbc94fe64c06d80
Malank Goel <goelmalank10@gmail.com>
2026-08-20 12:20:58 -0500

    Assert fixture identity helper export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -130,6 +130,7 @@ def test_shared_modules_export_expected_entry_points():
             "YES_SELECTION_PREFIX",
             "btts_selection_labels",
             "flag_bets",
+            "fixture_identity",
             "fixture_metadata_keys",
             "fixture_team_keys",
             "game_row_keys",

2c3f81d26b0784a9fdd842d10714202695bbe54f
Malank Goel <goelmalank10@gmail.com>
2026-08-29 20:41:29 -0500

    Assert game row model values export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -142,6 +142,7 @@ def test_shared_modules_export_expected_entry_points():
             "game_row_keys",
             "game_row_missing_keys",
             "game_row_model_keys",
+            "game_row_model_values",
             "game_row_sort_key",
             "game_key",
             "matches_fixture",

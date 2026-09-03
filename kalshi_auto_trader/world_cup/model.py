4e690147c0b2073a243f2a982d42c8021d934429
Malank Goel <goelmalank10@gmail.com>
2026-09-03 11:19:01 -0500

    Use btts model value in bet flagging

--- kalshi_auto_trader/world_cup/model.py
+++ kalshi_auto_trader/world_cup/model.py
@@ -819,7 +819,7 @@ def flag_bets(game: dict, odds: dict) -> list[Bet]:
     )
     if by is not None:
         b = _evaluate(BTTS_LINE, BTTS_YES_SELECTION, BTTS_NO_SELECTION, "",
-                      game[MODEL_BTTS_KEY], devig_binary(by, bn), by,
+                      btts_model_value(game), devig_binary(by, bn), by,
                       bn if bn is not None else probability.probability_complement(by))
         if b:
             bets.append(b)

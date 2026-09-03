cd17de339d2fa42c94546d822e035604d127b65d
Malank Goel <goelmalank10@gmail.com>
2026-09-03 11:13:55 -0500

    Assert totals model value export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -191,6 +191,7 @@ def test_shared_modules_export_expected_entry_points():
             "schedule_timing_values",
             "schedule_to_game_metadata_key_map",
             "strategy_line_names",
+            "totals_model_value",
             "totals_selection_labels",
             "winner_model_keys",
             "winner_model_values",

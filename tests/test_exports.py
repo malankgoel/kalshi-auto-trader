ea7bbaf9dcf3f999d8cac5f6a36e3529f797f91b
Malank Goel <goelmalank10@gmail.com>
2026-09-07 14:11:54 -0500

    Assert winner price key export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -229,6 +229,7 @@ def test_shared_modules_export_expected_entry_points():
             "resolve_order",
             "series_line_lookup",
             "team_key",
+            "winner_price_keys",
             "winner_series",
         },
         trader: {

23b022d1d0477d79c17f1dd0b170472ad371960c
Malank Goel <goelmalank10@gmail.com>
2026-09-07 14:14:19 -0500

    Assert totals price key export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -229,6 +229,7 @@ def test_shared_modules_export_expected_entry_points():
             "resolve_order",
             "series_line_lookup",
             "team_key",
+            "totals_price_keys",
             "winner_price_keys",
             "winner_series",
         },

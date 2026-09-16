ef1a652295fb49d11b33bd77f9a4f59822d0209a
Malank Goel <goelmalank10@gmail.com>
2026-09-15 23:39:33 -0500

    Assert winner line helper export

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -241,6 +241,7 @@ def test_shared_modules_export_expected_entry_points():
             "team_key",
             "totals_price_keys",
             "winner_index_key",
+            "winner_line_names",
             "winner_price_keys",
             "winner_series",
             "yes_ask_price_cents",
